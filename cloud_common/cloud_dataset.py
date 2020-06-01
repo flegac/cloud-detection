2afa56a70bc29fe98f9afbfdad21402ff49099cd
flegac <florent.legac@gmail.com>
2020-06-01 20:03:19 +0200

    feat: training

--- cloud_common/cloud_dataset.py
+++ cloud_common/cloud_dataset.py
@@ -1,14 +1,17 @@
 from enum import Enum
+from random import shuffle
+from typing import List, Tuple
 
 import cv2
 import numpy as np
 import pandas as pd
 import torch
+import torch.utils.data
 from fastai.vision import Image, SegmentationItemList
 
 from cloud_common.dataset import Sample, Dataset
 
-ROOT_PATH = '/DATADRIVE1/Datasets/38_cloud'
+ROOT_PATH = 'D:\\Datasets/38_cloud'
 
 
 class CLoudBand(Enum):
@@ -19,7 +22,9 @@ class CLoudBand(Enum):
     gt = "gt"
 
     def read(self, pattern: str, uri: str):
-        return cv2.imread(pattern.format(band=self.value, uri=uri), cv2.IMREAD_GRAYSCALE)
+        data = cv2.imread(pattern.format(band=self.value, uri=uri), cv2.IMREAD_GRAYSCALE)
+        data = (1 + data - data.min()) / (1 + data.max() - data.min())
+        return data
 
 
 class CloudSample(Sample):
@@ -39,29 +44,47 @@ class CloudSample(Sample):
         data = CLoudBand.gt.read(self.pattern, self.uri)
         if data is None:
             return None
-        data = np.dstack((data, 1 - data))
-        data = np.moveaxis(data, -1, 0)
+        # data = np.dstack((data, 1 - data))
+        # data = np.moveaxis(data, -1, 0)
+        data = np.expand_dims(data, 0)
         return data.astype(np.float)
 
 
 class CloudDataset(Dataset):
-    def __init__(self, dataset_id: str):
-        root_path = '/DATADRIVE1/Datasets/38_cloud/38-Cloud_{dataset_id}'.format(dataset_id=dataset_id)
+    def __init__(self, dataset_id: str, c: int = 1, indices: List[int] = None):
+        root_path = ROOT_PATH + '/38-Cloud_{dataset_id}'.format(dataset_id=dataset_id)
         self.pattern = root_path + '/{dataset_id}_{band}/{band}_{uri}.TIF'.replace('{dataset_id}', dataset_id)
         self.df = pd.read_csv(root_path + '/{dataset_id}_patches_38-Cloud.csv'.format(dataset_id=dataset_id))
         self.dataset_id = dataset_id
-        self.c = 2
+        self.c = c
+        self.indices = indices
 
     def __getitem__(self, index):
+        if self.indices:
+            index = self.indices[index]
         uri = self.df['name'][index]
         sample = CloudSample(pattern=self.pattern, uri=uri)
 
-        t1, t2 = torch.Tensor(sample.x()), torch.LongTensor(sample.y())
-
+        t1 = torch.tensor(sample.x(), dtype=torch.float32)
+        t2 = torch.tensor(sample.y(), dtype=torch.int64)
         return Image(t1), Image(t2)
 
     def __len__(self):
-        return 5 # len(self.df)
+        if self.indices:
+            return len(self.indices)
+        return len(self.df)
+
+    def split(self, train_ratio: float = .8, test_ratio: float = .2) -> Tuple['CloudDataset', 'CloudDataset']:
+        total = len(self)
+        test_size = int(total * test_ratio)
+        train_size = int(total * train_ratio)
+        indices = list(range(len(self)))
+        shuffle(indices)
+        train, test = indices[:train_size].copy(), indices[train_size:train_size + test_size].copy()
+        return (
+            CloudDataset(self.dataset_id, self.c, train),
+            CloudDataset(self.dataset_id, self.c, test)
+        )
 
 
 class CloudImageItemList(SegmentationItemList):
@@ -69,8 +92,8 @@ class CloudImageItemList(SegmentationItemList):
     def open(self, uri: str):
         uri = uri.replace('./', '')
         dataset_id = 'train'
-        root_path = '/DATADRIVE1/Datasets/38_cloud/38-Cloud_{dataset_id}'.format(dataset_id=dataset_id)
+        root_path = ROOT_PATH + '/38-Cloud_{dataset_id}'.format(dataset_id=dataset_id)
         pattern = root_path + '/{dataset_id}_{band}/{band}_{uri}.TIF'.replace('{dataset_id}', dataset_id)
 
         sample = CloudSample(pattern=pattern, uri=uri)
-        return Image(torch.Tensor(sample.x()))
+        return Image(torch.tensor(sample.x()))
--- cloud_train/train_script.py
+++ cloud_train/train_script.py
@@ -1,5 +1,5 @@
-import pandas as pd
-from fastai.vision import models, unet_learner, DataBunch, F
+import torch
+from fastai.vision import models, unet_learner, F, DataBunch
 
 from cloud_common.cloud_dataset import CloudDataset
 
@@ -15,34 +15,20 @@ def dice_metric(pred, targs, threshold=0):
 
 
 if __name__ == '__main__':
-    dataset_id = 'train'
-    root_path = '/DATADRIVE1/Datasets/38_cloud/38-Cloud_{dataset_id}'.format(dataset_id=dataset_id)
-    csv = '/{dataset_id}_patches_38-Cloud.csv'.format(dataset_id=dataset_id)
-    df = pd.read_csv(root_path + csv)
+    torch.cuda.empty_cache()
 
+    data = CloudDataset('train')
+    train, test = data.split(train_ratio=.8, test_ratio=.2)
+    print(len(train), len(test))
 
-    def label_func(x):
-        return root_path + '/train_gt/gt_{}.TIF'.format(x.replace('./', ''))
-
-
-    # imageList = CloudImageItemList.from_df(df, '.', cols='name')
-    # data = (imageList
-    #         .split_by_rand_pct()
-    #         .label_from_func(label_func, classes=['none', 'cloud'])
-    #         .databunch(bs=2, num_workers=2))
-
-    train = CloudDataset('train')
-    test = CloudDataset('test')
-    data = DataBunch.create(train, train, bs=1, num_workers=1)
+    data = DataBunch.create(train, train, bs=2, num_workers=1)
 
     learn = unet_learner(
         data=data, arch=models.resnet18,
         loss_func=bce_logits_floatify,
-        metrics=[dice_metric])
-
-    # learn.lr_find()
-    # learn.recorder.plot()
+        metrics=[dice_metric]
+    )
 
     print(learn.model[0][0])
     print(learn.model[-1][-1])
-    learn.fit(epochs=5)
+    learn.fit(epochs=20)
